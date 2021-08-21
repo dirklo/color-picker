4bf68131c15bf454eb395659142d0bdb7ffe5681
Rick Moore <dirklo@gmail.com>
2021-08-21 18:05:07 -0500

    add copy to clipboard

--- components/ColorBox.tsx
+++ components/ColorBox.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react'
 import React from 'react'
 import styles from '../styles/ColorBox.module.css'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 interface ColorBoxProps {
     color: {name: string; color: string}
@@ -8,18 +9,22 @@ interface ColorBoxProps {
 
 const ColorBox: FC<ColorBoxProps> = ({ color }) => {
     return (
-        <div style={{ background: color.color }} className={styles.ColorBox}>
-            <div className={styles.copy_container}>
+        <CopyToClipboard 
+            text={color.color}
+        >
+            <div style={{ background: color.color }} className={styles.ColorBox}>
+                <div className={styles.copy_container}>
 
+                </div>
+                <div className={styles.box_content}>
+                    <span>{color.name}</span>
+                </div>
+                <button className={styles.copy_button}>
+                    Copy
+                </button>
+                <span className={styles.see_more}>More</span>
             </div>
-            <div className={styles.box_content}>
-                <span>{color.name}</span>
-            </div>
-            <button className={styles.copy_button}>
-                Copy
-            </button>
-            <span className={styles.see_more}>More</span>
-        </div>
+        </CopyToClipboard>
     )
 }
 
