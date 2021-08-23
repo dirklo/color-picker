144add1b375c4c00dab6ff005f74ffa523bc2cd3
Rick Moore <dirklo@gmail.com>
2021-08-23 18:57:12 -0500

    add snackbar and styled select

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,14 @@
 import type { ChangeEvent, FC } from 'react' 
 import React, { useState } from 'react'
 import styles from '../styles/NavBar.module.css'
-import IsChromaPalette from './IsChromaPalette';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+
 
 interface NavBarProps {
     level: number;
@@ -15,8 +18,16 @@ interface NavBarProps {
 }
 
 const NavBar: FC<NavBarProps> = ({ level, setLevel, handleChange, format }): JSX.Element => {
+
+    const [ open, setOpen ] = useState(false)
+
+    const handleFormatChange = (event: ChangeEvent<{ value: unknown} >): void => {
+        handleChange(event)
+        setOpen(true)
+    }
+
     return (
-        <div className={styles.nav_bar}>
+        <header className={styles.nav_bar}>
             <div
                 className={styles.logo}
             >
@@ -41,13 +52,32 @@ const NavBar: FC<NavBarProps> = ({ level, setLevel, handleChange, format }): JSX
             <div
                 className={styles.select_container}
             >
-                <Select onChange={(event: ChangeEvent<{ value: unknown }>): void => handleChange(event)} value={format}>
+                <Select onChange={(event: ChangeEvent<{ value: unknown }>): void => handleFormatChange(event)} value={format}>
                     <MenuItem value='hex'>HEX - #ffffff</MenuItem>
                     <MenuItem value='rgb'>RGB - rgb(255, 255, 255)</MenuItem>
                     <MenuItem value='rgba'>RGBA - rgba(255, 255, 255, 0)</MenuItem>
                 </Select>
             </div>
-        </div>
+            <Snackbar 
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                onClose={() => setOpen(false)} 
+                open={open}
+                autoHideDuration={3000}
+                message={<span id={styles.message_id}>Format Changed To {format.toUpperCase()}</span>}
+                ContentProps={{
+                    'aria-describedby': 'message_id'
+                }}
+                action={[
+                    <IconButton 
+                        key='close' 
+                        onClick={() => setOpen(false)}
+                        aria-label='close'
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                ]}
+            />
+        </header>
     )
 }
 
