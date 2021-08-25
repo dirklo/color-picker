bccbeff2778f8c8621bbf8ef48a687322d43fff7
Rick Moore <dirklo@gmail.com>
2021-08-24 22:51:40 -0500

    convert sass and start stying palette list

--- components/PaletteList.tsx
+++ components/PaletteList.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react'
 import React from 'react'
 import IsPalette from './IsPalette'
 import MiniPalette from './MiniPalette'
+import styles from '../styles/PaletteList.module.scss'
 
 type PaletteListProps = {
     palettes: IsPalette[];
@@ -9,10 +10,23 @@ type PaletteListProps = {
 
 const PaletteList: FC<PaletteListProps> = ({ palettes }): JSX.Element => {
     return (
-        <div className='palette_list'>
-            {palettes.map(palette => 
-                <MiniPalette palette={palette} key={palette.id}/>
-            )}
+        <div className={styles.palette_list}>
+            <div
+                className={styles.container}
+            >
+            <nav
+                className={styles.nav}
+            >
+                <h2>React Colors</h2>
+            </nav>
+            </div>
+            <div
+                className={styles.palettes}
+            >
+                {palettes.map(palette => 
+                    <MiniPalette palette={palette} key={palette.id}/>
+                )}
+            </div>
         </div>  
     )
 }
