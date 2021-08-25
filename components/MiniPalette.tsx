7bb559eda972eb41c1ca963cd7096cfd0d2bf64c
Rick Moore <dirklo@gmail.com>
2021-08-24 23:18:15 -0500

    finish styling mini palette

--- components/MiniPalette.tsx
+++ components/MiniPalette.tsx
@@ -2,17 +2,28 @@ import type { FC } from 'react'
 import React from 'react'
 import IsPalette from './IsPalette'
 import styles from '../styles/MiniPalette.module.scss'
+import { Divider } from '@material-ui/core'
 
 type MiniPaletteProps = {
     palette: IsPalette;
 }
 
 const MiniPalette: FC<MiniPaletteProps> = ({ palette }) => {
+
+    const miniColorBoxes = palette.colors.map(color => 
+        <div 
+            style={{ backgroundColor: color.color }}
+            className={styles.mini_color} 
+            key={color.name}
+        />)
+    
     return (
         <div className={styles.mini_palette}>
             <div 
                 className={styles.colors}
-            ></div>
+            >
+                {miniColorBoxes}
+            </div>
             <h5
                 className={styles.title}
             >
