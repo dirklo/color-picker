fcf911a3a2bcf3dfed9dcf911148666c34b15e2f
Rick Moore <dirklo@gmail.com>
2021-08-29 22:31:56 -0500

    add tester files

---
+++ components/tester.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+function freeForAllFunction(args) {
+    return args;
+}
+function typeSafeFunction(args) {
+    return args;
+}
+function stringFunction(args) {
+    return args;
+}
+function numberFunction(args) {
+    return args;
+}
+console.log(typeSafeFunction(25));
+console.log(typeSafeFunction('Rick'));
+exports["default"] = typeSafeFunction;
