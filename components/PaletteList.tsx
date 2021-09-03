8fc2b4fb61de063c533d0825a31361c2e3e03f11
Rick Moore <dirklo@gmail.com>
2021-09-03 14:36:43 -0500

    add new palette form and link

--- components/PaletteList.tsx
+++ components/PaletteList.tsx
@@ -3,12 +3,15 @@ import React from 'react'
 import IsPalette from '../types/IsPalette'
 import MiniPalette from './MiniPalette'
 import styles from '../styles/PaletteList.module.scss'
+import { useRouter } from 'next/router'
 
 type PaletteListProps = {
     palettes: IsPalette[];
 }
 
 const PaletteList: FC<PaletteListProps> = ({ palettes }): JSX.Element => {
+
+    const router = useRouter()
 
     return (
         <div className={styles.palette_list}>
@@ -19,6 +22,15 @@ const PaletteList: FC<PaletteListProps> = ({ palettes }): JSX.Element => {
                     className={styles.nav}
                 >
                     <h2>React Colors</h2>
+                    <button 
+                        className={styles.new_palette_button}
+                        onClick={(e: React.MouseEvent): void => {
+                            e.preventDefault()
+                            router.push('/palettes/new')
+                        }}
+                    >
+                        Create Palette
+                    </button>
                 </nav>
                 <div
                     className={styles.palettes}
